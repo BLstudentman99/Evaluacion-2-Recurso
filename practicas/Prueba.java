e9191c52efe03e54d34878da6827cfc3f7dad94c
BLstudentman99 <143562661+BLstudentman99@users.noreply.github.com>
2024-03-04 16:50:08 -0600

    Add files via upload

---
+++ practicas/Prueba.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Prueba{
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println("introduce tu nombre: ");
+		String nombre = scanner.nextLine();
+		System.out.println(nombre);
+		
+      
+	}
+	
+
+
+
+
+}
